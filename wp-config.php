f52af1465cce8c6cef66d3633939e7f2ee43c780
root <watipqvp>
2018-03-01 20:13:09 -0800

    update

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,8 @@ define('DB_CHARSET', 'utf8mb4');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('WP_SITEURL','http://10.1.10.27');
+define('WP_HOME','http://10.1.10.27');
 /**#@+
  * Authentication Unique Keys and Salts.
  *
